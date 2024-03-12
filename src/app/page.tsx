1d018c560b46b73653201e68fd2184c2def7436f
imhobbang <dlaghgus2000@naver.com>
2024-03-12 19:45:25 +0900

    app route 베이스 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-"use client"
+//"use client"
 import Image from "next/image";
-import Header from "../app/Component/Header/Header";
-import Sidebar from "./Component/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <>
-      <Sidebar isOpen={false} setIsOpen={undefined}>
-      
-      </Sidebar>
+
     </>
   );
 }
